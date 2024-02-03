067ab3e7d03b391e6a63b0f07252fdf2e419f9f1
Hemang Vasu <hemang.vasu05@gmail.com>
2024-02-03 18:01:08 -0500

    Created "Recipe" Object Type

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes = [];
+  
+  constructor(){}
 
+  ngOnInIt(){
+
+  }
 }
